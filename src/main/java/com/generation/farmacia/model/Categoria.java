1ff62c27f3473dfed5069aae4556b0075f366290
Marcos <99marcoslopes@gmail.com>
2024-12-16 13:30:33 -0300

    CRUD de Categoria

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,62 @@
+package com.generation.farmacia.model;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "O Atributo Nome é obrigatório")
+	@Size(min = 2, max = 100, message = "O Atributo Nome de conter no mínimo 2 e no maximo 100 caracteres")
+	private String nome;
+
+	@NotNull(message = "O Atributo Descrição é obrigatório")
+	@Size(min = 10, max = 100, message = "O Atributo Descrição de conter no mínimo 10 e no maximo 100 caracteres")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
